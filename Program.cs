2e2a35ee5d424cefdbbdcb47433b448047c78c30
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-02-27 22:25:16 +0200

    ex 3 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System;
+
+public interface ICalc2
+{
+    int CountDistinct();
+    int EqualToValue(int valueToCompare);
+}
+
+public class Array : ICalc2
+{
+    private int[] numbers;
+    public Array(int[] numbers)
+    {
+        this.numbers = numbers;
+    }
+
+
+    public int CountDistinct()
+    {
+        return new HashSet<int>(numbers).Count;
+    }
+
+    public int EqualToValue(int valueToCompare)
+    {
+        int count = 0;
+        foreach (int num in numbers)
+        {
+            if (num == valueToCompare)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2,2,2,2, 3, 4, 5, 6, 7, 8, 9,9,9, 10 };
+        Array array = new Array(numbers);
+
+        Console.WriteLine(array.CountDistinct());
+        Console.WriteLine(array.EqualToValue(3));
+    }
+}
