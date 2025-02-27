bfba204903a3f4238b0c3f6ab046e1c8b9319095
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-02-27 21:50:24 +0200

    ex 1 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System;
+
+public interface ICalc
+{
+    int Less(int valueToCompare);
+    int Greater(int valueToCompare);
+}
+
+public class Array : ICalc
+{
+    private int[] numbers;
+    public Array(int[] numbers)
+    {
+        this.numbers = numbers;
+    }
+
+
+    public int Less(int valueToCompare)
+    {
+        int count = 0;
+        foreach(int i in numbers)
+        {
+            if (i< valueToCompare)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int Greater(int valueToCompare)
+    {
+        int count = 0;
+        foreach (int i in numbers)
+        {
+            if (i > valueToCompare)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Array array = new Array(numbers);
+ 
+        Console.WriteLine($"Less 6: {array.Less(6)}");
+        Console.WriteLine($"Greater 8: {array.Greater(8)}");
+    }
+}
